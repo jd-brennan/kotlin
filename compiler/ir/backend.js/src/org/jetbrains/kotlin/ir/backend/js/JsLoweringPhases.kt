83eb67e794560a9f0085e58e0fdb38eccd816cc4
Artem Kobzar <artem.kobzar@jetbrains.com>
2023-08-14 17:37:25 +0200

    Add lost lowering

--- compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/JsLoweringPhases.kt
+++ compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/JsLoweringPhases.kt
@@ -45,6 +45,12 @@ private val collectClassDefaultConstructorsPhase = makeIrModulePhase(
     description = "Collect classes default constructors to add it to metadata on code generating phase"
 )
 
+private val prepareCollectionsToExportLowering = makeIrModulePhase(
+    ::PrepareCollectionsToExportLowering,
+    name = "PrepareCollectionsToExportLowering",
+    description = "Add @JsTransitiveExport to exportable collections all the  declarations which we don't want to export such as `Enum.entries` or `DataClass::componentN`",
+)
+
 private val preventExportOfSyntheticDeclarationsLowering = makeIrModulePhase(
     ::ExcludeSyntheticDeclarationsFromExportLowering,
     name = "ExcludeSyntheticDeclarationsFromExportLowering",
@@ -796,6 +802,7 @@ val mainFunctionCallWrapperLowering = makeIrModulePhase<JsIrBackendContext>(
 val loweringList = listOf<SimpleNamedCompilerPhase<JsIrBackendContext, IrModuleFragment, IrModuleFragment>>(
     scriptRemoveReceiverLowering,
     validateIrBeforeLowering,
+    prepareCollectionsToExportLowering,
     preventExportOfSyntheticDeclarationsLowering,
     upgradeImplicitExportToExplicitLowering,
     removeImplicitExportIfItsNotReachableLowering,
