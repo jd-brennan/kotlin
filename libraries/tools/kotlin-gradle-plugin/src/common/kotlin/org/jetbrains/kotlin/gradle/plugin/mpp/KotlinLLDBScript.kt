44cac4e9999704c8df78907c542f6ea9c854cb63
Andrey Yastrebov <andrey.yastrebov@jetbrains.com>
2023-12-20 17:25:34 +0100

    fixup! KT-63592 Create setupLLDBScript task

--- libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/mpp/KotlinLLDBScript.kt
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/mpp/KotlinLLDBScript.kt
@@ -7,26 +7,52 @@ package org.jetbrains.kotlin.gradle.plugin.mpp
 
 import org.gradle.api.DefaultTask
 import org.gradle.api.Project
-import org.gradle.api.tasks.TaskProvider
+import org.gradle.api.file.ProjectLayout
+import org.gradle.api.model.ObjectFactory
+import org.gradle.api.provider.Property
+import org.gradle.api.tasks.*
 import org.jetbrains.kotlin.commonizer.toolsDir
 import org.jetbrains.kotlin.gradle.plugin.KotlinProjectSetupCoroutine
 import org.jetbrains.kotlin.gradle.targets.native.internal.konanDistribution
 import org.jetbrains.kotlin.gradle.tasks.locateOrRegisterTask
+import org.jetbrains.kotlin.gradle.utils.fileProperty
+import org.jetbrains.kotlin.gradle.utils.getFile
+import java.io.File
+import javax.inject.Inject
 
 internal val KotlinLLDBScriptSetupAction = KotlinProjectSetupCoroutine {
     locateOrRegisterLLDBScriptTask()
 }
 
-internal fun Project.locateOrRegisterLLDBScriptTask(): TaskProvider<DefaultTask> {
+abstract class LLDBInitTask
+@Inject constructor(
+    private val objects: ObjectFactory,
+    private val projectLayout: ProjectLayout,
+) : DefaultTask() {
+
+    @get:Input
+    internal abstract val fileName: Property<String>
+
+    @get:Input
+    internal abstract val konanToolsDir: Property<File>
+
+    @get:OutputFile
+    val outputFile: File
+        get() = projectLayout.buildDirectory.getFile().resolve(fileName.get())
+
+    @TaskAction
+    fun createScript() {
+        projectLayout
+            .file(objects.fileProperty(outputFile).asFile)
+            .getFile()
+            .writeText("command script import ${konanToolsDir.get().resolve("konan_lldb.py")}")
+    }
+}
+
+internal fun Project.locateOrRegisterLLDBScriptTask(): TaskProvider<LLDBInitTask> {
     return locateOrRegisterTask("setupLLDBScript") { task ->
         task.description = "Generate lldbinit file with imported konan_lldb.py script"
-        task.group = "other"
-        task.doFirst {
-            project
-                .apply {
-                    file("lldbinit")
-                        .writeText("command script import ${konanDistribution.toolsDir.resolve("konan_lldb.py")}")
-                }
-        }
+        task.fileName.set("lldbinit")
+        task.konanToolsDir.set(konanDistribution.toolsDir)
     }
 }
