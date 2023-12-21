909abe6f479fc3a02f5eec702e4da57af465b36e
Stanislav Ruban <stanislav.ruban@jetbrains.com>
2023-12-21 13:57:10 +0200

    Prepare test data for removal of separate test configurations for JDK 21
    ^KT-58765
    
    Now that JDK 21 was released as stable, we can remove separate test
    configurations for testing the compiler with JDK 21 and reintegrate
    the corresponding test data into standard test configurations.

--- compiler/testData/diagnostics/tests/testsWithJava21/newListMethods.fir.kt
+++ compiler/testData/diagnostics/tests/testsWithJava21/newListMethods.fir.kt
@@ -1,5 +1,4 @@
 // ISSUE: KT-58371
-// WITH_STDLIB
 // RENDER_DIAGNOSTICS_FULL_TEXT
 
 class A<T> : ArrayList<T>() {
@@ -11,8 +10,8 @@ class A<T> : ArrayList<T>() {
         super.addLast(t)
     }
 
-    override fun <!OVERRIDE_DEPRECATION!>getFirst<!>(): T = super.<!DEPRECATION!>getFirst<!>()
-    override fun <!OVERRIDE_DEPRECATION!>getLast<!>(): T = super.<!DEPRECATION!>getLast<!>()
+    override fun getFirst(): T = super.getFirst()
+    override fun getLast(): T = super.getLast()
 
     override fun removeFirst(): T = super.removeFirst()
     override fun removeLast(): T = super.removeLast()
@@ -23,11 +22,11 @@ class A<T> : ArrayList<T>() {
 fun foo(x: MutableList<String>, y: ArrayList<String>, z: A<String>) {
     x.addFirst("")
     x.addLast("")
-    x.<!DEPRECATION!>getFirst<!>()
-    x.<!DEPRECATION!>first<!> // synthetic property for getFirst()
+    x.<!UNRESOLVED_REFERENCE!>getFirst<!>()
+    x.<!FUNCTION_CALL_EXPECTED!>first<!> // synthetic property for getFirst()
     x.first() // stdlib extension on List
-    x.<!DEPRECATION!>getLast<!>()
-    x.<!DEPRECATION!>last<!>
+    x.<!UNRESOLVED_REFERENCE!>getLast<!>()
+    x.<!FUNCTION_CALL_EXPECTED!>last<!>
     x.last()
     x.<!DEBUG_INFO_CALL("fqName: kotlin.collections.MutableList.removeFirst; typeCall: function")!>removeFirst()<!>
     x.<!DEBUG_INFO_CALL("fqName: kotlin.collections.MutableList.removeLast; typeCall: function")!>removeLast()<!>
@@ -35,11 +34,11 @@ fun foo(x: MutableList<String>, y: ArrayList<String>, z: A<String>) {
 
     y.addFirst("")
     y.addLast("")
-    y.<!DEPRECATION!>getFirst<!>()
-    y.<!DEPRECATION!>first<!>
+    y.<!UNRESOLVED_REFERENCE!>getFirst<!>()
+    y.<!FUNCTION_CALL_EXPECTED!>first<!>
     y.first()
-    y.<!DEPRECATION!>getLast<!>()
-    y.<!DEPRECATION!>last<!>
+    y.<!UNRESOLVED_REFERENCE!>getLast<!>()
+    y.<!FUNCTION_CALL_EXPECTED!>last<!>
     y.last()
     y.<!DEBUG_INFO_CALL("fqName: java.util.ArrayList.removeFirst; typeCall: function")!>removeFirst()<!>
     y.<!DEBUG_INFO_CALL("fqName: java.util.ArrayList.removeLast; typeCall: function")!>removeLast()<!>
@@ -47,11 +46,11 @@ fun foo(x: MutableList<String>, y: ArrayList<String>, z: A<String>) {
 
     z.addFirst("")
     z.addLast("")
-    z.<!DEPRECATION!>getFirst<!>()
-    z.<!DEPRECATION!>first<!>
+    z.<!UNRESOLVED_REFERENCE!>getFirst<!>()
+    z.<!FUNCTION_CALL_EXPECTED!>first<!>
     z.first()
-    z.<!DEPRECATION!>getLast<!>()
-    z.<!DEPRECATION!>last<!>
+    z.<!UNRESOLVED_REFERENCE!>getLast<!>()
+    z.<!FUNCTION_CALL_EXPECTED!>last<!>
     z.last()
     z.<!DEBUG_INFO_CALL("fqName: A.removeFirst; typeCall: function")!>removeFirst()<!>
     z.<!DEBUG_INFO_CALL("fqName: A.removeLast; typeCall: function")!>removeLast()<!>
