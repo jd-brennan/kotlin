1c19ef33f562257494367f3008da4ea085b0bd31
Alexander Shabalin <alexander.shabalin@jetbrains.com>
2023-12-21 13:20:37 +0100

    fixup! [K/N][tests] Migrate link tests to new testing infra ^KT-61259

--- native/native.tests/testData/codegen/inline/lateinitProperty.kt
+++ native/native.tests/testData/codegen/inline/lateinitProperty.kt
@@ -1,3 +1,6 @@
+// KT-64511: lateinit is not lowered with caches
+// DISABLE_NATIVE: cacheMode=STATIC_EVERYWHERE
+// DISABLE_NATIVE: cacheMode=STATIC_PER_FILE_EVERYWHERE
 // MODULE: lib
 // FILE: lib.kt
 
